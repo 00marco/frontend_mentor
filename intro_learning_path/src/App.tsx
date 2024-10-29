f0eecc4cf381c5fa3fff605b16f1e4f2f3d5b3ff
Marco Pulido <marcopulido00@gmail.com>
2024-10-29 06:20:05 +0000

    added react

---
+++ intro_learning_path/src/App.tsx
@@ -0,0 +1,12 @@
+import QrCode from './components/qr_code'
+
+function App() {
+
+  return (
+    <>
+      <QrCode />
+    </>
+  )
+}
+
+export default App
---
+++ intro_learning_path/src/components/qr_code.tsx
@@ -0,0 +1,10 @@
+
+export default function QrCode() {
+  return (
+    <div className="grid grid-cols-1 grid-rows-3 content-center">
+      <text className="text-3xl font-bold underline text-center">Improve your front-end skills by building projects</text> 
+      <text className="text-center">Lorem ipsum dolor sit amet</text> 
+
+    </div>
+  )
+}
